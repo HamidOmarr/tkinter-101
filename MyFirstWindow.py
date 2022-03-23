47a3f9fc9671b4ae3de483f4298842a60e49f602
HamidOmarr <you@example.com>
2022-03-23 10:08:58 +0100

    'next'

--- MyFirstWindow.py
+++ MyFirstWindow.py
@@ -1,36 +1,15 @@
 import tkinter
 import time
 window = tkinter.Tk()
-window.geometry('0x0')
-def achtergrondGeel():
-    window.geometry('100x100')
-    window['background']='yellow'
-    print ('5')
-def achtergrondOrange():
-    window.geometry('150x150')
-    window['background']='orange'
-    print ('4')
-def achtergrondRood():
-    window.geometry('200x200')
-    window['background']='red' 
-    print ('3')
-def achtergrondPaars():
-    window.geometry('250x250')
-    window['background']='purple'
-    print ('2')
-def achtergrondZwart():
-    window.geometry('300x300')
-    window['background']='black'
-    print ('1')
-def WindowBoom():
-    window.destroy()
-    print('BOOM!')
+window.geometry('50x50')
+kleurlist = ['yellow','orange','red','purple','black']
+geometrty = ['50x50','100x100','150x150','200x200','250x250']
 
-window.after(1000,achtergrondGeel)
-window.after(2000,achtergrondOrange)
-window.after(3000,achtergrondRood)
-window.after(4000,achtergrondPaars)
-window.after(5000,achtergrondZwart)
-window.after(6000,WindowBoom)
+
+for x in range (len(kleurlist)):
+    window['background']=(kleurlist[x])
+    window.geometry(geometrty[x])
+    time.sleep(1)
+    window.update()
 
 window.mainloop()
